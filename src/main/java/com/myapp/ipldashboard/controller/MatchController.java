c354792dd6c867d8ddcd7dbe0014fd35a25b540b
gobi <abgopi@gmail.com>
2021-05-03 00:51:40 +0530

    [Added Project][Gobi]

---
+++ src/main/java/com/myapp/ipldashboard/controller/MatchController.java
@@ -0,0 +1,33 @@
+package com.myapp.ipldashboard.controller;
+
+import com.myapp.ipldashboard.model.Team;
+import com.myapp.ipldashboard.repository.MatchRepository;
+import com.myapp.ipldashboard.repository.TeamRepository;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class MatchController {
+
+    TeamRepository teamRepository;
+    MatchRepository matchRepository;
+
+public MatchController(TeamRepository teamRepository,MatchRepository matchRepository) {
+        this.teamRepository = teamRepository;
+        this.matchRepository = matchRepository;
+    }
+
+@GetMapping("/Team/{teamName}")
+public Team getTeamInfo(@PathVariable String teamName){
+    Team team = this.teamRepository.findByTeamName(teamName);
+    Pageable pageable = PageRequest.of(0,4);
+    team.setTeamMatches(this.matchRepository.getByTeam1OrTeam2OrderByDateDesc(teamName, teamName,pageable));
+    return team;
+}
+
+}
