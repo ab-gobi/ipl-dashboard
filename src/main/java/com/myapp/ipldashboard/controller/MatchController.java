d3f3aed009cd1ce0bf9e62376b2adc8ae1518df1
Gobi <abgopi@gmail.com>
2021-05-10 14:15:06 +0200

    [Added Match Page][Gobi]

--- src/main/java/com/myapp/ipldashboard/controller/MatchController.java
+++ src/main/java/com/myapp/ipldashboard/controller/MatchController.java
@@ -1,5 +1,9 @@
 package com.myapp.ipldashboard.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import com.myapp.ipldashboard.model.Match;
 import com.myapp.ipldashboard.model.Team;
 import com.myapp.ipldashboard.repository.MatchRepository;
 import com.myapp.ipldashboard.repository.TeamRepository;
@@ -9,6 +13,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -16,20 +21,27 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 public class MatchController {
 
-    TeamRepository teamRepository;
-    MatchRepository matchRepository;
+            TeamRepository teamRepository;
+            MatchRepository matchRepository;
 
-public MatchController(TeamRepository teamRepository,MatchRepository matchRepository) {
-        this.teamRepository = teamRepository;
-        this.matchRepository = matchRepository;
-    }
+        public MatchController(TeamRepository teamRepository,MatchRepository matchRepository) {
+                this.teamRepository = teamRepository;
+                this.matchRepository = matchRepository;
+            }
 
-@GetMapping("/team/{teamName}")
-public Team getTeamInfo(@PathVariable String teamName){
-    Team team = this.teamRepository.findByTeamName(teamName);
-    Pageable pageable = PageRequest.of(0,4);
-    team.setTeamMatches(this.matchRepository.getByTeam1OrTeam2OrderByDateDesc(teamName, teamName,pageable));
-    return team;
-}
+        @GetMapping("/team/{teamName}")
+        public Team getTeamInfo(@PathVariable String teamName){
+            Team team = this.teamRepository.findByTeamName(teamName);
+            Pageable pageable = PageRequest.of(0,4);
+            team.setTeamMatches(this.matchRepository.getByTeam1OrTeam2OrderByDateDesc(teamName, teamName,pageable));
+            return team;
+        }
+
+        @GetMapping("/team/{teamName}/matches")
+        public List<Match> getMatchInfo(@PathVariable String teamName,@RequestParam int year){
+            LocalDate startDate = LocalDate.of(year, 1, 1);
+            LocalDate endDate = LocalDate.of(year+1, 1, 1);
+            return this.matchRepository.getMatchesByTeamBetweenDates(teamName, startDate, endDate);
+        }
 
 }
