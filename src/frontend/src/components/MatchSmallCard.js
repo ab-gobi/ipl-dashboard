137656e9a4279195a9923536bae9be7c51910704
Gobi <abgopi@gmail.com>
2021-05-12 19:12:48 +0200

    [Team Page Styling][Gobi]

--- src/frontend/src/components/MatchSmallCard.js
+++ src/frontend/src/components/MatchSmallCard.js
@@ -1,13 +1,15 @@
 import { React } from 'react';
 import { Link } from 'react-router-dom';
+import './MatchSmallCard.scss';
 
 export const MatchSmallCard = ({match,teamName}) => {
   const otherTeam = match.team1 === teamName ? match.team2 : match.team1;
   const otherTeamRoute = `/teams/${otherTeam}`;
+  const isMatchWon = teamName === match.matchWinner;
     return (
-      <div className="MatchSmallCard">
-        <h3>vs 
-           <Link to={otherTeamRoute}>{otherTeam}</Link></h3>
+      <div className= { isMatchWon ? 'MatchSmallCard won-card' : 'MatchSmallCard lost-card'} >
+        <span>vs</span> 
+        <h1><Link to={otherTeamRoute}>{otherTeam}</Link></h1>
         <p>{match.matchWinner} won by {match.resultMargin} {match.result}</p>
       </div>
     );
